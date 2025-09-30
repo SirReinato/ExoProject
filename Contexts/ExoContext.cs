8ad410d1226f2274aafb98f45f7991f2c6808662
SirReinato <renatofranca2881@gmail.com>
2025-09-30 18:48:42 -0300

    Projeto iniciado

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,24 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext() { }
+
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // String de conexão para SQL Server Express
+                optionsBuilder.UseSqlServer(
+                    "Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;");
+            }
+        }
+
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
