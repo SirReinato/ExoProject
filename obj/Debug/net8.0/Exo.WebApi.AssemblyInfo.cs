d5e22b2a9f7339ab5b1d8f4ae6f4baaba820bc9d
SirReinato <renatofranca2881@gmail.com>
2025-09-30 22:21:48 -0300

    atividade 5

--- obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11cb3311f982c2a95b2632f437c18d32f6cac51b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c9e97d8cba95d572b215557289c31765334152")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
